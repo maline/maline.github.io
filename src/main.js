87fbab4a1bb5bf9f0b8f493e1a396e151a30e1ae
Malin <>
2018-01-22 14:44:45 -0800

    removing router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+// import VueRouter from 'vue-router'
 import App from './App.vue'
 import HomeView from './components/Home'
 import PortfolioView from './components/Portfolio'
