e1cf4213f072d24c0550296dac69103e65b91b27
Malin <>
2018-01-23 16:43:04 -0800

    added analytics

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAnalytics from 'vue-analytics'
 // import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(VueAnalytics, {
+  id: 'UA-112980288-'
+})
 
 /* eslint-disable no-new */
 new Vue({
